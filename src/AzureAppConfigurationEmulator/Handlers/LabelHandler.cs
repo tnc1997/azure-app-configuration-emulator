3eba4b9bc7c382450bfa758abe2fe9692937e8e4
Thomas Clark <tnc1997@virginmedia.com>
2023-10-29 13:45:39 +0000

    feat: add list configuration setting labels endpoint (#18)

---
+++ src/AzureAppConfigurationEmulator/Handlers/LabelHandler.cs
@@ -0,0 +1,39 @@
+using System.Text.RegularExpressions;
+using AzureAppConfigurationEmulator.Constants;
+using AzureAppConfigurationEmulator.Contexts;
+using AzureAppConfigurationEmulator.Extensions;
+using AzureAppConfigurationEmulator.Results;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AzureAppConfigurationEmulator.Handlers;
+
+public class LabelHandler
+{
+    public static async Task<Results<LabelSetResult, InvalidCharacterResult, TooManyValuesResult>> List(
+        [FromServices] ApplicationDbContext context,
+        CancellationToken cancellationToken,
+        [FromQuery] string name = LabelFilter.Any)
+    {
+        if (name != LabelFilter.Any)
+        {
+            if (new Regex(@"(?=.*(?<!\\),)(?=.*\*)").IsMatch(name))
+            {
+                return new InvalidCharacterResult(nameof(name));
+            }
+
+            if (new Regex(@"(?:.*(?<!\\),){5,}").IsMatch(name))
+            {
+                return new TooManyValuesResult(nameof(name));
+            }
+        }
+
+        var labels = await context.ConfigurationSettings
+            .Where(label: name)
+            .Select(setting => setting.Label)
+            .ToListAsync(cancellationToken);
+
+        return new LabelSetResult(labels);
+    }
+}
---
+++ src/AzureAppConfigurationEmulator/Results/LabelSetResult.cs
@@ -0,0 +1,34 @@
+namespace AzureAppConfigurationEmulator.Results;
+
+public class LabelSetResult(IEnumerable<string> labels) :
+    IResult,
+    IContentTypeHttpResult,
+    IStatusCodeHttpResult,
+    IValueHttpResult,
+    IValueHttpResult<LabelSet>
+{
+    public async Task ExecuteAsync(HttpContext httpContext)
+    {
+        if (StatusCode.HasValue)
+        {
+            httpContext.Response.StatusCode = StatusCode.Value;
+        }
+
+        if (Value is not null)
+        {
+            await httpContext.Response.WriteAsJsonAsync(Value, options: default, ContentType);
+        }
+    }
+
+    public string? ContentType => "application/vnd.microsoft.appconfig.labelset+json";
+
+    public int? StatusCode => StatusCodes.Status200OK;
+
+    object? IValueHttpResult.Value => Value;
+
+    public LabelSet? Value { get; } = new(labels.Select(label => new Label(label)));
+}
+
+public record LabelSet(IEnumerable<Label> Items);
+
+public record Label(string Name);
