25328c18ffa4515b53515e518bff3b2fc9848b40
Thomas Clark <tnc1997@virginmedia.com>
2023-11-12 18:57:56 +0000

    feat: add support for https using ssl certificate (#24)

--- src/AzureAppConfigurationEmulator/Extensions/HostingExtensions.cs
+++ src/AzureAppConfigurationEmulator/Extensions/HostingExtensions.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography.X509Certificates;
 using AzureAppConfigurationEmulator.Contexts;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,6 +6,28 @@ namespace AzureAppConfigurationEmulator.Extensions;
 
 public static class HostingExtensions
 {
+    public static string SslCertificatePath { get; } = Environment.OSVersion.Platform switch
+    {
+        PlatformID.Win32S => @"C:\ProgramData\Azure App Configuration Emulator\emulator.crt",
+        PlatformID.Win32Windows => @"C:\ProgramData\Azure App Configuration Emulator\emulator.crt",
+        PlatformID.Win32NT => @"C:\ProgramData\Azure App Configuration Emulator\emulator.crt",
+        PlatformID.WinCE => @"C:\ProgramData\Azure App Configuration Emulator\emulator.crt",
+        PlatformID.Unix => "/usr/local/share/azureappconfigurationemulator/emulator.crt",
+        PlatformID.MacOSX => "/usr/local/share/azureappconfigurationemulator/emulator.crt",
+        _ => throw new ArgumentOutOfRangeException()
+    };
+
+    public static string SslCertificateKeyPath { get; } = Environment.OSVersion.Platform switch
+    {
+        PlatformID.Win32S => @"C:\ProgramData\Azure App Configuration Emulator\emulator.key",
+        PlatformID.Win32Windows => @"C:\ProgramData\Azure App Configuration Emulator\emulator.key",
+        PlatformID.Win32NT => @"C:\ProgramData\Azure App Configuration Emulator\emulator.key",
+        PlatformID.WinCE => @"C:\ProgramData\Azure App Configuration Emulator\emulator.key",
+        PlatformID.Unix => "/usr/local/share/azureappconfigurationemulator/emulator.key",
+        PlatformID.MacOSX => "/usr/local/share/azureappconfigurationemulator/emulator.key",
+        _ => throw new ArgumentOutOfRangeException()
+    };
+
     public static string DatabasePath { get; } = Environment.OSVersion.Platform switch
     {
         PlatformID.Win32S => @"C:\ProgramData\Azure App Configuration Emulator\emulator.db",
@@ -15,6 +38,20 @@ public static class HostingExtensions
         PlatformID.MacOSX => "/var/lib/azureappconfigurationemulator/emulator.db",
         _ => throw new ArgumentOutOfRangeException()
     };
+
+    public static IWebHostBuilder ConfigureKestrel(this IWebHostBuilder builder)
+    {
+        return builder.ConfigureKestrel(options =>
+        {
+            if (File.Exists(SslCertificatePath) && File.Exists(SslCertificateKeyPath))
+            {
+                options.ConfigureHttpsDefaults(options =>
+                {
+                    options.ServerCertificate = X509Certificate2.CreateFromPemFile(SslCertificatePath, SslCertificateKeyPath);
+                });
+            }
+        });
+    }
 
     public static void InitializeDatabase(this IApplicationBuilder app)
     {
